5f11cb7cc2e0281130305807ae5d6be9b0ad4ff1
Velimir Stojanov <velimirstojanovmkd2016@gmail.com>
2021-03-24 10:35:31 +0100

    HelloWorld added

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("---------------");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("    *");
+            Console.WriteLine("   ***");
+            Console.WriteLine("  *****");
+            Console.WriteLine(" *******");
+            Console.WriteLine("*********");
+
+            Console.WriteLine("---------------");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("    *");
+            Console.WriteLine("   ***");
+            Console.WriteLine("  * * *");
+            Console.WriteLine("    *");
+            Console.WriteLine("   * *");
+            Console.WriteLine("  *   *");
+
+            Console.WriteLine("---------------");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine("Velimir Stojanov, student at SEDC");
+            Console.ReadLine();
+        }
+    }
+}
