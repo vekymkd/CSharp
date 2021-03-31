de9ed31e748db3aca626b8f84a60b69c7ecd91b5
Velimir Stojanov <velimirstojanovmkd2016@gmail.com>
2021-03-31 15:27:06 +0200

    Adding SumOfEven and StudentsGroups

---
+++ Basic C#/StudentGroup/StudentGroup/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace StudentGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+
+                string[] studentsG1 = new string[] { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+                string[] studentsG2 = new string[] { "Velimir", "Slave", "Aleksandar", "Goran", "Vlatko" };
+
+                Console.WriteLine("Press 1 for Group 1, press 2 for Group 2"); string selectedGroup = Console.ReadLine();
+
+                if (selectedGroup == "1")
+                {
+                    Console.WriteLine("The students of Group 1 are: ");
+                    foreach (string student in studentsG1)
+                    {
+                        Console.WriteLine(student);
+                    }
+                }
+                else if (selectedGroup == "2")
+                {
+                    Console.WriteLine("The students of Group 2 are: ");
+                    foreach (string student in studentsG2)
+                    {
+                        Console.WriteLine(student);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Wrong input value, please try again!");
+                }
+
+            }
+        }
+    }
+}
---
+++ Basic C#/SumOfEven/SumOfEven/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SumOfEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] integerArray = new int[6];
+            int sum = 0;
+
+            for (int i = 0; i < integerArray.Length; i++)
+            {
+                Console.Write("Enter integer: "); string numberInput = Console.ReadLine();
+                bool conversionToNumber = int.TryParse(numberInput, out int num);
+
+                if (!conversionToNumber)
+                {
+                    i--;
+                    continue;
+                }
+
+                integerArray[i] = num;
+            }
+
+            foreach (int number in integerArray)
+            {
+                if (number % 2 == 0)
+                {
+                    sum += number;
+                }
+            }
+
+            Console.WriteLine($"The result is: {sum}.");
+        }
+    }
+}
