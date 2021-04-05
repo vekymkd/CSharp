69160b65e8b4cccc339fe737578bcd19ccdb58b6
Velimir Stojanov <velimirstojanovmkd2016@gmail.com>
2021-04-05 09:06:40 +0200

    Substring folder added

---
+++ Basic C#/Substrings/Substrings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Substrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            string userInput = Console.ReadLine();
+
+            bool isValidNumber = int.TryParse(userInput, out int number);
+
+            if (isValidNumber)
+            {
+                string result = Substring("Hello from SEDC Code Academy", number);
+                Console.WriteLine(result);
+                Console.WriteLine($"The number of characters of the new string is {result.Length}");
+            }
+        }
+        static string Substring(string input, int numberOfWords)
+        {
+            return input.Substring(0, numberOfWords);
+        }
+    }
+}
