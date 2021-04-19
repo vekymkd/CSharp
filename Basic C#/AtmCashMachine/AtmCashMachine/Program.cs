f94146ccffe0c604103c325e0c339a75149b702e
Velimir Stojanov <velimirstojanovmkd2016@gmail.com>
2021-04-19 13:19:38 +0200

    ATM app

---
+++ Basic C#/AtmCashMachine/AtmCashMachine/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Models;
+
+namespace AtmCashMachine
+{
+    class Program
+    {
+        static Customer FindCustomer(Customer[] customers, string cardNumber)
+        {
+            foreach (Customer c in customers)
+            {
+                if (c.CardNumber == cardNumber)
+                {
+                    return c;
+                }
+            }
+            return null;
+        }
+
+        static void Main(string[] args)
+        {
+            Customer[] customers = new Customer[] {
+                new Customer("Velimir","Stojanov", "3293829398239238293", 34562, 2300),
+                new Customer("Risto","Panchevski", "8235823582358235", 45000, 1500),
+                new Customer("Adis","Drndar", "03243482882774426", 23240, 1200),
+                new Customer("Petko","Petkovski", "0000220311012203", 30000, 400)
+            };
+
+            Console.WriteLine("Welcome to our ATM");
+            Console.WriteLine("Please enter card number: ");
+            string cardNumber = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(cardNumber))
+            {
+                Console.WriteLine("Please enter card number: ");
+            }
+            else
+            {
+                Customer customer = FindCustomer(customers, cardNumber);
+                if (customer == null)
+                {
+                    Console.WriteLine("Customer not found!");
+                    return;
+                }
+                Console.WriteLine("Please enter pin number: ");
+                bool success = int.TryParse(Console.ReadLine(), out int pin);
+                if (success)
+                {
+                    if (customer.VerifyPinNumber(pin))
+                    {
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect pin!");
+                        return;
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ Basic C#/AtmCashMachine/Models/Customer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Models
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CardNumber { get; set; }
+        private int PinNumber { get; set; }
+        private double AccountBalance { get; set; }
+
+        public Customer(string firstName, string lastName, string cardNumber, int pin, double balance)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            CardNumber = cardNumber;
+            PinNumber = pin;
+            AccountBalance = balance;
+        }
+        public double GetAccountBalance()
+        {
+            return AccountBalance;
+        }
+
+        public bool VerifyPinNumber(int pin)
+        {
+            return PinNumber == pin;
+        }
+
+        public void DepositAmount(int amount)
+        {
+            AccountBalance += amount;
+        }
+
+        public bool WithdrawAmount(int amount)
+        {
+            if (amount > AccountBalance)
+            {
+                return false;
+            }
+                
+            AccountBalance -= amount;
+            return true;
+        }
+    }
+}
